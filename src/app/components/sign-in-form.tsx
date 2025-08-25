60347bff8d5fce40820482d19c59996253153974
slothmast3r <oskar.straszynski@gmail.com>
2025-08-25 14:53:53 +0200

    some styling

---
+++ src/app/components/sign-in-form.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { signIn } from "next-auth/react";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+const SignInSchema = z.object({
+  email: z.string().email(),
+});
+
+export function SignIn() {
+  const form = useForm<z.infer<typeof SignInSchema>>({
+    resolver: zodResolver(SignInSchema),
+    defaultValues: { email: "" },
+  });
+
+  async function onSubmit(values: z.infer<typeof SignInSchema>) {
+    await signIn("resend", { email: values.email });
+  }
+
+  return (
+    <Card className="min-w-lg">
+      <CardHeader>
+        <CardTitle>Login to your account</CardTitle>
+        <CardDescription>
+          Enter your email below to login to your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input type="email" placeholder="Email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Sign in with Resend</Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
-import { SignIn } from "./components/sign-in";
+import { SignIn } from "./components/sign-in-form";
 import { auth } from "@/auth";
 import SignOut from "./components/sign-out";
 
 export default async function Home() {
   const session = await auth();
-  
+
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <SignIn />
       {session?.user ? (
         <div>Signed in as {session.user.id}</div>
